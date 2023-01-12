ab21674c6041750244aec4a54a9af41d52e28d38
Alberth <alberthhls@protonmail.com>
2023-01-12 11:29:43 -0300

    fix: generate random number in user name

--- core.php
+++ core.php
@@ -3,7 +3,7 @@
  * Plugin Name: Guru WebHooks Integration
  * Plugin URI:
  * Description: Create a custom route to handle guru webhook, create users by webhook events
- * Version: 1.0.5
+ * Version: 1.0.7.1
  * Author: Alberth henrique
  * Author URI: https://github.com/D7alth/
  */
@@ -25,15 +25,20 @@ function create_guru_user_endpoint() {
 function create_guru_user( $request ) {
     $data = array();
     $params = $request->get_params();
-
-    $name = $params['contact']['name'];
-    $email = $params['contact']['email'];
+    // split params into wp create users params   
+    $name = $params['subscriber']['name'];
+    $email = $params['subscriber']['email'];
     //$pass = $email;
+
     $pass = wp_generate_password( 12, true );
-    
+
+    // santize fields
     $name_sanitize = sanitize_text_field($name);
     $email_sanitize = sanitize_email($email);
-    
+
+    $name_sanitize .= ' - ' . rand(1, 9999);
+
+    // create user
     $new_user_id = wp_create_user($name_sanitize, $email_sanitize, $email_sanitize);
     
     if(is_wp_error( $new_user_id )){
@@ -41,16 +46,17 @@ function create_guru_user( $request ) {
         $data['status'] = 'ERROR';
         
         $data['message'] = "Error when trying to create user : " . $new_user_id->get_error_message(); 
+        http_response_code(500); 
         return $data;
     }else{
         
         $data['received_data'] = array(
-            'name' => $name, 
-            'email' => $email
+            'name' => $name_sanitize, 
+            'email' => $email_sanitize
             );
             
         $data['message'] = "User successfully registered! : " . $new_user_id;
-
+        http_response_code(201);
         return $data;
     }
 }
